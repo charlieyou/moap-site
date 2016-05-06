8da755b35d8d4ea05e19dde4fef864ea5cd8f5e4
Charlie You <charlie@charlieyou.me>
2016-05-06 16:08:56 -0400

    fa

--- solve.php
+++ solve.php
@@ -50,7 +50,7 @@
               <p>Your personal algorithm design assistant</p>
 <?php
 $cmd = 'java -jar ~/MOAP.jar ' . $_POST["moap-input"] . " 2>&1";
-echo "Problem: ".$POST["moap-input"];
+echo "Problem: ".$_POST["moap-input"];
 echo "<pre>".shell_exec($cmd)."</pre>";
 ?>
                         </center>
